9d1c7548cd843a2a54a252f00ab67f292dba575a
Erica Zhong <ez8@rice.edu>
2019-09-30 15:38:02 -0500

    Basic websocket implementation. Uses client2.go

--- server/apiserver/apiserver.go
+++ server/apiserver/apiserver.go
@@ -2,12 +2,15 @@ package apiserver
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 func Hello(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Fprintf(w, "hello\n")
+	fmt.Fprintf(w, "ERICA\n")
 }
 
 func Headers(w http.ResponseWriter, req *http.Request) {
@@ -15,6 +18,33 @@ func Headers(w http.ResponseWriter, req *http.Request) {
 	for name, headers := range req.Header {
 		for _, h := range headers {
 			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func Echo(w http.ResponseWriter, r *http.Request) {
+
+	// fmt.Fprintf(w, "ECHO\n")
+
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("server read:", err)
+			break
+		}
+		log.Printf("server recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("server write:", err)
+			break
 		}
 	}
 }
--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/rgreen312/owlplace/server
 
 go 1.12
+
+require github.com/gorilla/websocket v1.4.1
