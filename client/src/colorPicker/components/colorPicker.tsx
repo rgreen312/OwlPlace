a53296f9bf867b56f627e1b51437c5c3b58f0d56
alvinmagee <awm5@rice.edu>
2019-10-17 16:12:54 -0500

    changes

--- client/src/colorPicker/components/colorPicker.tsx
+++ client/src/colorPicker/components/colorPicker.tsx
@@ -1,16 +1,44 @@
 import React, { FC } from "react";
 import { SketchPicker } from "react-color";
+import { Button } from "antd";
+import { Redirect } from "react-router-dom";
 
 interface Props {
-  colorPicked: () => void; 
+  colorPicked: (Color?) => void; 
 }
 // http://casesandberg.github.io/react-color/
 
 const ColorPicker: FC<Props> = ({colorPicked}) => {
+  let chosenColor = null; 
+
+  const handleChangeComplete = (color) => {
+    chosenColor = color;
+    console.log(chosenColor); 
+  };
+
+  const colorChosen = () => {
+    console.log(chosenColor); 
+    console.log("okay Pressed")
+    colorPicked(chosenColor)
+  }
+
+  const cancel = () => {
+    console.log("cancelling"); 
+    return <Redirect to='/'/>; 
+  }
+
   return (
-    <SketchPicker 
-      onChangeComplete={colorPicked}      
-    />
+    <div>
+      <SketchPicker 
+        onChange={handleChangeComplete}      
+      />
+      <Button onClick={colorChosen}>
+        Okay
+      </Button>
+      <Button onClick={cancel}>
+        Cancel
+      </Button>
+    </div>
   );
 };
 
