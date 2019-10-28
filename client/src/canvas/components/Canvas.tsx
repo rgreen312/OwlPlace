a65ee31f0272cec4675f85b897c3d9ac0f45f4e8
Namanh Kapur <namanhkapur1205@gmail.com>
2019-10-28 01:36:20 -0500

    playing around with zoom + pan

--- client/src/canvas/components/Canvas.tsx
+++ client/src/canvas/components/Canvas.tsx
@@ -1,9 +1,9 @@
-import React, { Component, createRef, RefObject } from 'react';
-import ColorPicker from '../../colorPicker/components/colorPicker';
-import { Redirect } from 'react-router-dom';
-import './Canvas.scss';
-import { Icon } from 'antd';
-import { ZOOM_CHANGE_FACTOR } from '../constants';
+import React, { Component, createRef, RefObject } from "react";
+import ColorPicker from "../../colorPicker/components/colorPicker";
+import { Redirect } from "react-router-dom";
+import "./Canvas.scss";
+import { Icon } from "antd";
+import { ZOOM_CHANGE_FACTOR } from "../constants";
 
 interface Props {
   receivedError: boolean;
@@ -16,49 +16,204 @@ interface Props {
 
 class Canvas extends Component<Props> {
   canvasRef: RefObject<HTMLCanvasElement>;
+  mouseDown: Boolean;
+  widthCanvas: any;
+  heightCanvas: any;
+  xleftView: any;
+  ytopView: any;
+  widthViewOriginal: any;
+  heightViewOriginal: any;
+  widthView: any;
+  heightView: any;
+  lastX: any;
+  lastY: any;
 
   constructor(props) {
     super(props);
     this.canvasRef = createRef();
+    this.mouseDown = false;
+    this.widthCanvas = 0;
+    this.heightCanvas = 0;
+    this.xleftView = 0;
+    this.ytopView = 0;
+    this.widthViewOriginal = 1.0; //actual width and height of zoomed and panned display
+    this.heightViewOriginal = 1.0;
+    this.widthView = this.widthViewOriginal; //actual width and height of zoomed and panned display
+    this.heightView = this.heightViewOriginal;
+    this.lastX = 0;
+    this.lastY = 0;
   }
 
   componentDidMount() {
+    this.setup();
+  }
+
+  setup() {
     this.canvasRef.current!.width = 1000;
     this.canvasRef.current!.height = 1000;
-
-    const context = this.canvasRef.current!.getContext('2d');
-    // const image = new Image();
-
-    // image.onload = function() {
-    //   if (context) {
-    //     context.drawImage(image, 0, 0);
-    //   }
-    // };
-    // image.src = this.props.initialImage;
-
+    this.widthCanvas = this.canvasRef.current!.width;
+    this.heightCanvas = this.canvasRef.current!.height;
+    const context = this.canvasRef.current!.getContext("2d");
 
     if (context) {
       this.props.registerContext(context);
     }
 
-    context!.imageSmoothingEnabled = false;
+    this.canvasRef.current!.addEventListener(
+      "dblclick",
+      this.handleDblClick,
+      false
+    ); // dblclick to zoom in at point, shift dblclick to zoom out.
+    this.canvasRef.current!.addEventListener(
+      "mousedown",
+      this.handleMouseDown,
+      false
+    ); // click and hold to pan
+    this.canvasRef.current!.addEventListener(
+      "mousemove",
+      this.handleMouseMove,
+      false
+    );
+    this.canvasRef.current!.addEventListener(
+      "mouseup",
+      this.handleMouseUp,
+      false
+    );
+    this.canvasRef.current!.addEventListener(
+      "mousewheel",
+      this.handleMouseWheel,
+      false
+    );
 
-    // TODO: remove this code
-    context!.fillStyle ='#000000';
-    context!.fillRect(0, 0, 1000, 500);
-    context!.fillStyle = '#ff0000';
-    context!.fillRect(0, 500, 1000, 500);
+    this.draw(context);
+  }
 
-    context!.scale(100, 100);
+  draw(ctx) {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.scale(
+      this.widthCanvas / this.widthView,
+      this.heightCanvas / this.heightView
+    );
+    ctx.translate(-this.xleftView, -this.ytopView);
 
-    this.canvasRef.current!.addEventListener('mousemove', (ev) => {
-      const { x, y } = this.getMousePos(this.canvasRef.current, ev);
-      this.props.updatePosition(x, y);
-    }, false);
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(
+      this.xleftView,
+      this.ytopView,
+      this.widthView,
+      this.heightView
+    );
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0.1, 0.5, 0.1, 0.1);
+    ctx.fillStyle = "red";
+    ctx.fillRect(0.3, 0.2, 0.4, 0.2);
+    ctx.fillStyle = "green";
+    ctx.beginPath();
+    ctx.arc(
+      this.widthView / 2 + this.xleftView,
+      this.heightView / 2 + this.ytopView,
+      0.05,
+      0,
+      360,
+      false
+    );
+    ctx.fill();
+  }
 
-    this.canvasRef.current!.addEventListener('mouseout', () => {
-      this.props.onMouseOut();
-    })
+  handleDblClick(event) {
+    var X =
+      event.clientX -
+      this.canvasRef.current!.offsetLeft -
+      this.canvasRef.current!.clientLeft +
+      this.canvasRef.current!.scrollLeft; //Canvas coordinates
+    var Y =
+      event.clientY -
+      this.canvasRef.current!.offsetTop -
+      this.canvasRef.current!.clientTop +
+      this.canvasRef.current!.scrollTop;
+    var x = (X / this.widthCanvas) * this.widthView + this.xleftView; // View coordinates
+    var y = (Y / this.heightCanvas) * this.heightView + this.ytopView;
+
+    var scale = event.shiftKey == 1 ? 1.5 : 0.5; // shrink (1.5) if shift key pressed
+    this.widthView *= scale;
+    this.heightView *= scale;
+
+    if (
+      this.widthView > this.widthViewOriginal ||
+      this.heightView > this.heightViewOriginal
+    ) {
+      this.widthView = this.widthViewOriginal;
+      this.heightView = this.heightViewOriginal;
+      x = this.widthView / 2;
+      y = this.heightView / 2;
+    }
+
+    this.xleftView = x - this.widthView / 2;
+    this.ytopView = y - this.heightView / 2;
+
+    const context = this.canvasRef.current!.getContext("2d");
+
+    this.draw(context);
+  }
+
+  handleMouseDown(event) {
+    this.mouseDown = true;
+  }
+
+  handleMouseUp(event) {
+    this.mouseDown = false;
+  }
+
+  handleMouseMove(event) {
+    var X =
+      event.clientX -
+      this.canvasRef.current!.offsetLeft -
+      this.canvasRef.current!.clientLeft +
+      this.canvasRef.current!.scrollLeft;
+    var Y =
+      event.clientY -
+      this.canvasRef.current!.offsetTop -
+      this.canvasRef.current!.clientTop +
+      this.canvasRef.current!.scrollTop;
+
+    if (this.mouseDown) {
+      var dx = ((X - this.lastX) / this.widthCanvas) * this.widthView;
+      var dy = ((Y - this.lastY) / this.heightCanvas) * this.heightView;
+      this.xleftView -= dx;
+      this.ytopView -= dy;
+    }
+    this.lastX = X;
+    this.lastY = Y;
+
+    const context = this.canvasRef.current!.getContext("2d");
+
+    this.draw(context);
+  }
+
+  handleMouseWheel(event) {
+    var x = this.widthView / 2 + this.xleftView; // View coordinates
+    var y = this.heightView / 2 + this.ytopView;
+
+    var scale = event.wheelDelta < 0 || event.detail > 0 ? 1.1 : 0.9;
+    this.widthView *= scale;
+    this.heightView *= scale;
+
+    if (
+      this.widthView > this.widthViewOriginal ||
+      this.heightView > this.heightViewOriginal
+    ) {
+      this.widthView = this.widthViewOriginal;
+      this.heightView = this.heightViewOriginal;
+      x = this.widthView / 2;
+      y = this.heightView / 2;
+    }
+
+    // scale about center of view, rather than mouse position. This is different than dblclick behavior.
+    this.xleftView = x - this.widthView / 2;
+    this.ytopView = y - this.heightView / 2;
+    const context = this.canvasRef.current!.getContext("2d");
+
+    this.draw(context);
   }
 
   getMousePos(canvas, evt) {
@@ -73,21 +228,24 @@ class Canvas extends Component<Props> {
     const { receivedError, zoomFactor, setZoomFactor } = this.props;
     return (
       // receivedError ? <Redirect to='/error'/> :
-      <div className='canvas-container'>
+      <div className="canvas-container">
         {/* <ColorPicker
           onCancel={() => console.log('canceled')}
           onComplete={(c) => console.log('color selected: ', c)}
         /> */}
-        <div className='zoom-canvas' style={{ transform: `scale(${zoomFactor}, ${zoomFactor})` }}>
+        <div
+          className="zoom-canvas"
+          //   style={{ transform: `scale(${zoomFactor}, ${zoomFactor})` }}
+        >
           <canvas ref={this.canvasRef} />
         </div>
-        <div className='zoom-controls'>
+        <div className="zoom-controls">
           <Icon
-            type='plus-circle'
+            type="plus-circle"
             onClick={() => setZoomFactor(zoomFactor + ZOOM_CHANGE_FACTOR)}
           />
           <Icon
-            type='minus-circle'
+            type="minus-circle"
             onClick={() => setZoomFactor(zoomFactor - ZOOM_CHANGE_FACTOR)}
           />
         </div>
