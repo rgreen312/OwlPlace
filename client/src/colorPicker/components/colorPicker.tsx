257a565d754916a9fc45a322b53157eb5843b16f
alvinmagee <awm5@rice.edu>
2019-11-03 15:33:41 -0600

    working

--- client/src/colorPicker/components/colorPicker.tsx
+++ client/src/colorPicker/components/colorPicker.tsx
@@ -11,22 +11,27 @@ interface RGBColor {
 }
 
 interface Props {
-  onComplete: (color: RGBColor) => void;
+  onColorChange: (color:RGBColor) => void; 
+  onComplete: () => void;
   onCancel: () => void;
   className?: string;
   style?: CSSProperties;
 }
 // http://casesandberg.github.io/react-color/
 
-const ColorPicker: FC<Props> = ({ onComplete, onCancel, className, style }) => {
+const ColorPicker: FC<Props> = ({ onColorChange, onComplete, onCancel, className, style }) => {
   const [color, setColor] = useState({ r: 0, g: 0, b: 0 })
-  const complete = () => onComplete(color);
+  const complete = () => onComplete();
+
+  const onColorSelection = (color) => {
+    onColorChange(color); 
+  }
 
   return ( 
     <div className={className} style={style}>
       <SketchPicker 
         color={color}
-        onChange={(c) => setColor({ r: c.rgb.r, b: c.rgb.b, g: c.rgb.g })}
+        onChange={(c) => onColorSelection({ r: c.rgb.r, b: c.rgb.b, g: c.rgb.g })}
       />
       <div className='button-bar'>
         <Button onClick={complete} className='okay-button'>
