2acf0d0a5cb6b9f676520064f227bd47a1277c93
Namanh Kapur <namanhkapur1205@gmail.com>
2019-11-04 18:16:59 -0600

    added loading state

--- client/src/canvas/components/Canvas.tsx
+++ client/src/canvas/components/Canvas.tsx
@@ -1,14 +1,15 @@
-import React, { Component, createRef, RefObject, useState } from 'react';
-import ColorPicker from '../../colorPicker/components/colorPicker';
-import { Redirect } from 'react-router-dom';
-import './Canvas.scss';
-import { Icon } from 'antd';
-import { ZOOM_CHANGE_FACTOR } from '../constants';
-import { Color, RGBColor } from 'react-color';
-import classNames from 'classnames';
+import React, { Component, createRef, RefObject, useState } from "react";
+import ColorPicker from "../../colorPicker/components/colorPicker";
+import { Redirect } from "react-router-dom";
+import "./Canvas.scss";
+import { Icon, Spin } from "antd";
+import { ZOOM_CHANGE_FACTOR } from "../constants";
+import { Color, RGBColor } from "react-color";
+import classNames from "classnames";
 
 interface Props {
   receivedError: boolean;
+  isLoading: boolean;
   registerContext: (context: CanvasRenderingContext2D) => void;
   updatePosition: (x: number, y: number) => void;
   position: { x: number; y: number };
@@ -20,7 +21,7 @@ interface Props {
 
 interface State {
   showColorPicker: boolean;
-  previousColor: RGBColor | null; 
+  previousColor: RGBColor | null;
   isDrag: boolean;
   translateX: number;
   translateY: number;
@@ -38,18 +39,18 @@ class Canvas extends Component<Props, State> {
     this.canvasRef = createRef();
     this.state = {
       showColorPicker: false,
-      previousColor: null, 
+      previousColor: null,
       isDrag: false,
       translateX: 0,
       translateY: 0,
       dragStartX: 0,
       dragStartY: 0,
       colorPickerX: 0,
-      colorPickerY: 0,
+      colorPickerY: 0
     };
     this.onCancel = this.onCancel.bind(this);
     this.onComplete = this.onComplete.bind(this);
-    this.onColorChange = this.onColorChange.bind(this); 
+    this.onColorChange = this.onColorChange.bind(this);
     this.updateTranslate = this.updateTranslate.bind(this);
   }
 
@@ -57,7 +58,7 @@ class Canvas extends Component<Props, State> {
     this.canvasRef.current!.width = 1000;
     this.canvasRef.current!.height = 1000;
 
-    const context = this.canvasRef.current!.getContext('2d');
+    const context = this.canvasRef.current!.getContext("2d");
 
     // const image = new Image();
 
@@ -75,24 +76,24 @@ class Canvas extends Component<Props, State> {
     context!.imageSmoothingEnabled = false;
 
     // TODO: remove this code
-    context!.fillStyle = '#000000';
+    context!.fillStyle = "#000000";
     context!.fillRect(0, 0, 1000, 500);
-    context!.fillStyle = '#ff0000';
+    context!.fillStyle = "#ff0000";
     context!.fillRect(0, 500, 1000, 500);
 
-    this.canvasRef.current!.addEventListener('mousemove', ev => {
+    this.canvasRef.current!.addEventListener("mousemove", ev => {
       if (this.state.showColorPicker) return;
       const { x, y } = this.getMousePos(this.canvasRef.current, ev);
       this.props.updatePosition(x, y);
     });
 
-    this.canvasRef.current!.addEventListener('mouseout', () => {
+    this.canvasRef.current!.addEventListener("mouseout", () => {
       if (this.state.showColorPicker) return;
       this.props.onMouseOut();
     });
 
     // On mousedown, get the current location to be used for dragging
-    this.canvasRef.current!.addEventListener('mousedown', e => {
+    this.canvasRef.current!.addEventListener("mousedown", e => {
       const { zoomFactor } = this.props;
       const { translateX, translateY } = this.state;
       const startPositionX = e.clientX - translateX;
@@ -101,7 +102,7 @@ class Canvas extends Component<Props, State> {
       let pickerX = e.clientX + zoomFactor;
       let pickerY = e.clientY;
 
-      // The color picker is 220 x 337 pixels 
+      // The color picker is 220 x 337 pixels
       if (pickerX + 220 > window.innerWidth) {
         pickerX -= 220;
       }
@@ -118,7 +119,7 @@ class Canvas extends Component<Props, State> {
 
       // If the user moves after clicking, then they are dragging so we add listener
       this.canvasRef.current!.addEventListener(
-        'mousemove',
+        "mousemove",
         this.updateTranslate
       );
     });
@@ -126,13 +127,13 @@ class Canvas extends Component<Props, State> {
     /**
      * On mouse up, we check if the user was dragging. If they were, we set isDrag to false and
      * remove the event listener for dragging.
-     * 
+     *
      * If they were not dragging, then we display the color picker so we can update the color of
      * the pixel.
      */
-    this.canvasRef.current!.addEventListener('mouseup', ev => {
+    this.canvasRef.current!.addEventListener("mouseup", ev => {
       this.canvasRef.current!.removeEventListener(
-        'mousemove',
+        "mousemove",
         this.updateTranslate
       );
 
@@ -143,15 +144,21 @@ class Canvas extends Component<Props, State> {
       }
 
       const { x, y } = this.getMousePos(this.canvasRef.current, ev);
-      const imageData = this.canvasRef.current!.getContext('2d')!.getImageData(x, y, 1, 1)
+      const imageData = this.canvasRef
+        .current!.getContext("2d")!
+        .getImageData(x, y, 1, 1);
       this.setState({
-        previousColor: {r: imageData.data[1], g: imageData.data[2], b: imageData.data[3]} 
+        previousColor: {
+          r: imageData.data[1],
+          g: imageData.data[2],
+          b: imageData.data[3]
+        }
       });
-      this.showColorPicker(); 
+      this.showColorPicker();
 
       this.setState({
         isDrag: false
-      })
+      });
     });
   }
 
@@ -163,7 +170,7 @@ class Canvas extends Component<Props, State> {
     this.setState({
       isDrag: true,
       translateX: x,
-      translateY: y,
+      translateY: y
     });
   }
 
@@ -184,10 +191,10 @@ class Canvas extends Component<Props, State> {
   }
 
   onColorChange(c: RGBColor) {
-    const context = this.canvasRef.current!.getContext('2d');
+    const context = this.canvasRef.current!.getContext("2d");
     const x = this.props.position.x - 1;
     const y = this.props.position.y - 1;
-    context!.fillStyle = `rgb(${c.r}, ${c.g}, ${c.b})`
+    context!.fillStyle = `rgb(${c.r}, ${c.g}, ${c.b})`;
     context!.fillRect(x, y, 1, 1);
     this.props.onUpdatePixel({ r: c.r, g: c.g, b: c.b }, x, y);
   }
@@ -205,56 +212,81 @@ class Canvas extends Component<Props, State> {
 
     // We should change the color back if cancel was pressed.
     if (didCancel) {
-      const context = this.canvasRef.current!.getContext('2d');
+      const context = this.canvasRef.current!.getContext("2d");
 
-      const x = this.props.position.x; 
+      const x = this.props.position.x;
       const y = this.props.position.y;
-      const c = this.state.previousColor!; 
+      const c = this.state.previousColor!;
 
-      context!.fillStyle = 'rgb(' + c.r + ',' + c.g + ',' + c.b + ')'
+      context!.fillStyle = "rgb(" + c.r + "," + c.g + "," + c.b + ")";
       context!.fillRect(x - 1, y - 1, 1, 1);
 
-      this.props.onUpdatePixel({ r: c.r, g: c.g, b: c.b}, x, y);
+      this.props.onUpdatePixel({ r: c.r, g: c.g, b: c.b }, x, y);
     }
   }
 
   render() {
     const { receivedError, zoomFactor, setZoomFactor } = this.props;
-    const { translateX, translateY, isDrag, colorPickerX, colorPickerY } = this.state;
+    const isLoading = true;
+    const {
+      translateX,
+      translateY,
+      isDrag,
+      colorPickerX,
+      colorPickerY
+    } = this.state;
     return (
       // receivedError ? <Redirect to='/error'/> :
-      <div className='canvas-container'>
-        {this.state.showColorPicker && (
+      <div>
+        {isLoading && <Spin className='spinner-style' size='large' />}
+        <div
+          className={classNames({
+            "hide-canvas": isLoading,
+            "canvas-container": true
+          })}
+        >
+          {this.state.showColorPicker && (
             <ColorPicker
-              onColorChange={(c) => this.onColorChange(c)}
+              onColorChange={c => this.onColorChange(c)}
               onCancel={this.onCancel}
               onComplete={this.onComplete}
-              className='color-picker' 
-              style={{ top: `${colorPickerY}px`, left: `${colorPickerX}px`}}
+              className='color-picker'
+              style={{ top: `${colorPickerY}px`, left: `${colorPickerX}px` }}
             />
-        )}
-        <div
-          className={classNames({ 'pan-canvas': true, 'drag-canvas': isDrag })}
-          style={{ transform: `translate(${translateX}px, ${translateY}px)` }}
-        >
+          )}
+
           <div
-            className='zoom-canvas'
-            style={{ transform: `scale(${zoomFactor}, ${zoomFactor})` }}
+            className={classNames({
+              "pan-canvas": true,
+              "drag-canvas": isDrag
+            })}
+            style={{ transform: `translate(${translateX}px, ${translateY}px)` }}
           >
-            <canvas ref={this.canvasRef} />
+            <div
+              className='zoom-canvas'
+              style={{ transform: `scale(${zoomFactor}, ${zoomFactor})` }}
+            >
+              <canvas ref={this.canvasRef} />
+            </div>
           </div>
-        </div>
-        <div className='zoom-controls'>
-          <Icon
-            type='plus-circle'
-            onClick={() => { setZoomFactor(zoomFactor + ZOOM_CHANGE_FACTOR); this.onCancel() }}
-            className='zoom-icon'
-          />
-          <Icon
-            type='minus-circle'
-            onClick={() => { setZoomFactor(zoomFactor - ZOOM_CHANGE_FACTOR); this.onCancel() }}
-            className='zoom-icon'
-          />
+          <div className='zoom-controls'>
+            <Icon
+              type='plus-circle'
+              onClick={() => {
+                setZoomFactor(zoomFactor + ZOOM_CHANGE_FACTOR);
+                this.onCancel();
+              }}
+              className='zoom-icon'
+            />
+            <Icon
+              type='minus-circle'
+              onClick={() => {
+                setZoomFactor(zoomFactor - ZOOM_CHANGE_FACTOR);
+                this.onCancel();
+              }}
+              className='zoom-icon'
+            />
+          </div>
         </div>
       </div>
     );
--- client/src/websocket/reducers.ts
+++ client/src/websocket/reducers.ts
@@ -1,37 +1,47 @@
-import { createReducer } from '../createReducer';
-import * as ActionTypes from './actionTypes';
-import { ConnectSuccess } from './actions';
-import { combineReducers } from 'redux';
- 
+import { createReducer } from "../createReducer";
+import * as ActionTypes from "./actionTypes";
+import { ConnectSuccess } from "./actions";
+import { combineReducers } from "redux";
+
 export interface State {
   socket: WebSocket | null;
   isConnected: boolean;
-  receivedError: boolean; 
+  receivedError: boolean;
+  isLoading: boolean;
 }
 
-const socket = createReducer<State['socket']>(null, {
+const socket = createReducer<State["socket"]>(null, {
   [ActionTypes.StartConnect]: () => null,
   [ActionTypes.ConnectError]: () => null,
   [ActionTypes.CloseConnection]: () => null,
-  [ActionTypes.ConnectSuccess]: (state, action: ConnectSuccess) => action.payload.socket,
+  [ActionTypes.ConnectSuccess]: (state, action: ConnectSuccess) =>
+    action.payload.socket
 });
 
-const isConnected = createReducer<State['isConnected']>(false, {
+const isConnected = createReducer<State["isConnected"]>(false, {
   [ActionTypes.StartConnect]: () => false,
   [ActionTypes.ConnectError]: () => false,
   [ActionTypes.CloseConnection]: () => false,
-  [ActionTypes.ConnectSuccess]: () => true,
+  [ActionTypes.ConnectSuccess]: () => true
 });
 
-const receivedError = createReducer<State['receivedError']>(false, {
+const receivedError = createReducer<State["receivedError"]>(false, {
   [ActionTypes.StartConnect]: () => false,
   [ActionTypes.ConnectError]: () => true,
   [ActionTypes.CloseConnection]: () => false,
-  [ActionTypes.ConnectSuccess]: () => false,
-}); 
+  [ActionTypes.ConnectSuccess]: () => false
+});
+
+const isLoading = createReducer<State["isLoading"]>(false, {
+  [ActionTypes.StartConnect]: () => true,
+  [ActionTypes.ConnectError]: () => false,
+  [ActionTypes.CloseConnection]: () => false,
+  [ActionTypes.ConnectSuccess]: () => false
+});
 
 export default combineReducers({
   socket,
   isConnected,
-  receivedError
-})
+  receivedError,
+  isLoading
+});
