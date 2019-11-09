242e323c8c6f8f46b52540a305d1d382152408eb
alvinmagee <awm5@rice.edu>
2019-11-09 15:15:06 -0600

    styling

--- client/src/testing/components/Testing.tsx
+++ client/src/testing/components/Testing.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const Testing: FC<Props> = ({sendUpdateMessage, sendLoginMessage}) => (
   <div>
-    
+    <h2>TESTING AREA</h2>
     <p>Click "Pixel 1" to send an update message to the server!</p>
     <button onClick = {() => sendUpdateMessage("user1", 10, 400, 255, 255, 255)} id="p1"> Pixel 1 </button>
     <button onClick = {() => sendLoginMessage("testemail@gmail.com")} id="login"> User Login </button>
