25f1f115fc3abf10c61d7575b541842dc1d2c31d
ryangreen <ryan.green0@me.com>
2019-11-09 13:31:03 -0600

    Updates

---
+++ precache-manifest.55f4a8314d447c485acf72915715ddbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a87432a6ab34ad42bf5b7808d7b9bd",
+    "url": "/OwlPlace/index.html"
+  },
+  {
+    "revision": "7cf7724a78809087d7df",
+    "url": "/OwlPlace/static/css/2.ca0e2e9d.chunk.css"
+  },
+  {
+    "revision": "d5d00366e0a158d0e596",
+    "url": "/OwlPlace/static/css/main.d88c43fe.chunk.css"
+  },
+  {
+    "revision": "7cf7724a78809087d7df",
+    "url": "/OwlPlace/static/js/2.648872b9.chunk.js"
+  },
+  {
+    "revision": "d5d00366e0a158d0e596",
+    "url": "/OwlPlace/static/js/main.fe888f98.chunk.js"
+  },
+  {
+    "revision": "3ea7f19c38ed191bfe19",
+    "url": "/OwlPlace/static/js/runtime-main.99db3b79.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/OwlPlace/precache-manifest.85dd2fad07f029f49999a89376582232.js"
+  "/OwlPlace/precache-manifest.55f4a8314d447c485acf72915715ddbb.js"
 );
 
 self.addEventListener('message', (event) => {
