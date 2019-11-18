cf4242bd5997be4c1d7ceca06f441a26154f39de
Patrick Brady <pj.brady97@gmail.com>
2019-11-18 16:13:40 -0600

    fixing minor diff

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,4 @@
-// export const HOSTNAME = '127.0.0.1:3001';
-export const HOSTNAME = '192.168.99.100:3001';
+export const HOSTNAME = '127.0.0.1:3001';
+// export const HOSTNAME = '192.168.99.100:3001';
 // The above line should be uncommented and replaced with the 
 // docker-machine ip on windows computers
